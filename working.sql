8129f8e4c882f40ea6a6fabac463728f302db3b0
navin-ranjan <navinranjan15498@gmail.com>
2026-02-04 05:13:19 +0000

    topic aaded

---
+++ working.sql
@@ -0,0 +1,38 @@
+-- rohit.rajput@nucleusteq.com   --indore https://www.linkedin.com/feed/update/urn:li:activity:7424094896569008128/
+
+-- https://southasiacareers.deloitte.com/job/Bengaluru-Technology-&-Transformation-Engineering-ConsultantSenior-Consultant-Azure-Data-Engineer/52155144/
+
+------
+
+complete febric module
+
+--------------
+
+Hi Manpreet,
+I hope you're doing well. I'm currently seeking new opportunities in Data Engineering.
+
+I have 3.5 years of experience building ETL pipelines using Azure tools (Data Factory, Databricks, ADLS Gen2, Synapse Analytics), along with SQL, PySpark, and Python. I’m also skilled in Hadoop, Spark, DSA, and Airflow for orchestration.
+
+If there's a relevant opening in your organization, I’d be truly grateful for a referral. Please find my resume attached for your reference.
+
+Thank you for your time!
+Best regards,
+Navin Ranjan
+
+
+-----------
+
+skill
+
+Azure Databricks, Azure Data Factory, Azure Synapse Analytics, ADLS Gen2, Apache Spark, PySpark, Databricks, Hadoop, HDFS, Python,SQL, Apache Airflow, S3, MinIO, Cassandra(NoSQL), MySQL, AzureDevOps(CI/CD),ETL/ELTDataPipeline, Star Schema, Git, Power BI, Kafka
+
+leaving reason
+
+The role offered limited learning opportunities, so I’m seeking a position to enhance my skills and work with modern technologies aligned with my long-term goals.
+
+
+------------
+
+
+https://medium.com/@krthiak
+
